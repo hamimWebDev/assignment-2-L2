3f3491a9e30c79d455945c97e77b7c7bc6f12db9
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-23 11:37:31 +0600

    product model create successfully

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -13,5 +11,5 @@ exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
 exports.app.use("/api/products", product_route_1.productsRoute);
 exports.app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
